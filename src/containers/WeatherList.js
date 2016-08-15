e9a4a5828396d947341b6a1e1eb18703e113c339
Emma J <codemzy@gmail.com>
2016-08-15 12:02:21 +0000

    Updated WeatherList to display city from state

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class WeatherList extends React.Component {
     render () {
+        let weatherList = this.props.weather.map((cityData, index) => {
+            return (
+                <tr key={index}>
+                    <td>{cityData.city.name}</td>
+                </tr>
+            );
+        });
         return (
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
                     </tr>
                 </thead>
                 <tbody>
+                    {weatherList}
                 </tbody>
             </table>
         );
     }
 }
 
-export default WeatherList;
+function mapStateToProps(state) {
+    return {weather: state.weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
