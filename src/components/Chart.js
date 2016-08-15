1e4b38b694f5e254a8a77dce256a2f7d805333aa
Emma J <codemzy@gmail.com>
2016-08-15 12:41:49 +0000

    Updated Chart to a functional component

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import {Sparklines, SparklinesLine} from 'react-sparklines';
 
-class Chart extends React.Component {
-    
-    render() {
-        return (
-            <Sparklines height={120} width={180} data={this.props.data}>
-                <SparklinesLine color={this.props.color} />
-            </Sparklines>
-        );
-    }
-}
-
-export default Chart;
+export default (props) => {
+    return (
+        <Sparklines height={120} width={180} data={props.data}>
+            <SparklinesLine color={props.color} />
+        </Sparklines>
+    );
+};
